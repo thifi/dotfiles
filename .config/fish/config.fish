8948b03305a6130de61bbdf5d05606630725e6a2
Itay Garin <thifixp@gmail.com>
2020-05-02 20:41:51 +0300

    Add some more aliases to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,13 @@ set -g fish_key_bindings fish_vi_key_bindings
 bind -M insert \cF forward-word
 bind -M insert \cB backward-word
 bind -M insert \cJ end-of-line
+bind -M insert \cA beginning-of-line
+bind -M insert \cP history-search-backward
+bind -M insert \cN history-search-forward
+
+## ALIASES
+alias q="exit"
+alias bd="bg && disown"
+alias b="bat"
+alias c="bat"
+alias nb="python3 /home/itayg/workspace/NextSilicone/omp-playground/nextsilicon_build.py"
