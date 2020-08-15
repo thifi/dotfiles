e156a6b8acddafcbe5896ce5caad9fce7bf7046a
Itay Garin <thifixp@gmail.com>
2020-08-15 16:21:08 +0300

    Comment out ktrl's setup for now

--- setup.sh
+++ setup.sh
@@ -13,16 +13,16 @@ cd -
 
 # ktrl setup
 
-rm -f /opt/ktrl/*.ron
-sudo rm -f /usr/local/bin/womier*
-sudo rm -f /etc/udev/rules.d/80-womier.rules
+# rm -f /opt/ktrl/*.ron
+# sudo rm -f /usr/local/bin/womier*
+# sudo rm -f /etc/udev/rules.d/80-womier.rules
 
-ln -s ~/dotfiles/ktrl/default.ron /opt/ktrl/default.ron
-ln -s ~/dotfiles/ktrl/womier.ron /opt/ktrl/womier.ron
-ln -s ~/dotfiles/ktrl/default.ron /opt/ktrl/cfg.ron
+# ln -s ~/dotfiles/ktrl/default.ron /opt/ktrl/default.ron
+# ln -s ~/dotfiles/ktrl/womier.ron /opt/ktrl/womier.ron
+# ln -s ~/dotfiles/ktrl/default.ron /opt/ktrl/cfg.ron
 
-sudo ln -s ~/dotfiles/ktrl/udev/womier-udev-add.sh /usr/local/bin/womier-udev-add.sh
-sudo ln -s ~/dotfiles/ktrl/udev/womier-udev-remove.sh /usr/local/bin/womier-udev-remove.sh
-sudo ln -s ~/dotfiles/ktrl/udev/80-womier.rules /etc/udev/rules.d/80-womier.rules
+# sudo ln -s ~/dotfiles/ktrl/udev/womier-udev-add.sh /usr/local/bin/womier-udev-add.sh
+# sudo ln -s ~/dotfiles/ktrl/udev/womier-udev-remove.sh /usr/local/bin/womier-udev-remove.sh
+# sudo ln -s ~/dotfiles/ktrl/udev/80-womier.rules /etc/udev/rules.d/80-womier.rules
 
 echo "DONE!"
