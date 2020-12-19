9c84e1d63ff86bea819e2bb7826978e42e2b19cf
Itay Garin <thifixp@gmail.com>
2020-12-19 20:07:41 +0200

    home-manager: Add a few more tools

--- home-manager/.config/nixpkgs/home.nix
+++ home-manager/.config/nixpkgs/home.nix
@@ -15,5 +15,28 @@
     pkgs.vim
     pkgs.emacs
     pkgs.google-chrome
+    pkgs.alacritty
+    pkgs.qbittorrent
+    pkgs.keepass
+
+    pkgs.fzf
+    pkgs.fasd
+    pkgs.ripgrep
+    pkgs.bat
+    pkgs.fd
+    pkgs.nixfmt
   ];
+
+  programs.fish = {
+    enable = true;
+    plugins = [{
+      name = "fasd";
+      src = pkgs.fetchFromGitHub {
+        owner = "oh-my-fish";
+        repo = "plugin-fasd";
+        rev = "38a5b6b6011106092009549e52249c6d6f501fba";
+        sha256 = "06v37hqy5yrv5a6ssd1p3cjd9y3hnp19d3ab7dag56fs1qmgyhbs";
+      };
+    }];
+  };
 }
