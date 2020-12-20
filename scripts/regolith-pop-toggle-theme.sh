ae539bb095b7d1a4409574689a7387cebfbeb333
Itay Garin <thifixp@gmail.com>
2020-12-20 21:59:26 +0200

    regolith: Add light-dark theme toggle

---
+++ scripts/regolith-pop-toggle-theme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+STATE_FILE="/tmp/theme.state"
+STATE=`cat $STATE_FILE`
+
+if [ $STATE == "Pop" ]; then
+    echo "Changing to Regolith"
+    /usr/share/rofi/modi/look-selector.sh lascaille
+    echo "Regolith" > $STATE_FILE
+else
+    echo "Changing to Pop"
+    gsettings set org.gnome.desktop.interface gtk-theme Pop
+    echo "Pop" > $STATE_FILE
+fi
