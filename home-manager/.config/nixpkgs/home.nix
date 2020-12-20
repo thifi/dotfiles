506d01967bec1d8f700461b1fe6ebbed6a44bfaf
Itay Garin <thifixp@gmail.com>
2020-12-20 23:46:58 +0200

    home-manager: Lots of fish in the sea

--- home-manager/.config/nixpkgs/home.nix
+++ home-manager/.config/nixpkgs/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   nixpkgs.config.allowUnfree = true;
@@ -9,6 +9,8 @@
   home.username = "itayg";
   home.homeDirectory = "/home/itayg";
   home.stateVersion = "21.03";
+
+  home.sessionVariables = { EDITOR = "vim"; };
 
   home.packages = [
     pkgs.meld
@@ -37,12 +39,48 @@
     enable = true;
     plugins = [
       {
+        name = "bass";
+        src = pkgs.fetchFromGitHub {
+          owner = "edc";
+          repo = "bass";
+          rev = "df4a1ebf8c0536e4bd7b7828a4c0dcb2b7b5d22b";
+          sha256 = "1dgydrza6lvx3dl9spkla1g728x5rr76mqrwk2afrl732439y6jl";
+        };
+      }
+      {
         name = "fasd";
         src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
           repo = "plugin-fasd";
           rev = "38a5b6b6011106092009549e52249c6d6f501fba";
           sha256 = "06v37hqy5yrv5a6ssd1p3cjd9y3hnp19d3ab7dag56fs1qmgyhbs";
+        };
+      }
+      {
+        name = "fish_logo";
+        src = pkgs.fetchFromGitHub {
+          owner = "laughedelic";
+          repo = "fish_logo";
+          rev = "dc6a40836de8c24c62ad7c4365aa9f21292c3e6e";
+          sha256 = "09lvjjwajiysd5y4wvl3iq3md4bzi5f69kwb4xdvgr6s8yvx158d";
+        };
+      }
+      {
+        name = "aws";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-aws";
+          rev = "25fa5c44f988dd46e77f9cdebbe4074ce77e97d4";
+          sha256 = "03yqrb1l7wmp5hzig9klf9gsdg8ncrky0bkvynzy8jxy9yvhpg7j";
+        };
+      }
+      {
+        name = "bang-bang";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-bang-bang";
+          rev = "d45ae216969fa5c3eac0b6248119e8a1da56fe89";
+          sha256 = "0jpcs8fpw9a69ai6mwhgikw77j03fhnixcl21yx1b5p65333pddc";
         };
       }
       {
@@ -55,7 +93,21 @@
         };
       }
     ];
+
+    shellAliases = { bd = "bg && disown"; };
+
+    functions = {
+      fish_greeting = "fish_logo";
+      gitignore = "curl -sL https://www.gitignore.io/api/$argv";
+    };
   };
+
+  xdg.configFile."fish/conf.d/plugin-theme-robbyrussell.fish".text =
+    lib.mkAfter ''
+      for f in $plugin_dir/*.fish
+        source $f
+      end
+    '';
 
   programs.git = {
     enable = true;
@@ -84,6 +136,23 @@
     enable = true;
     enableFishIntegration = true;
     verbs = [
+      {
+        invocation = "p";
+        execution = ":parent";
+      }
+      {
+        invocation = "edit";
+        shortcut = "e";
+        execution = "$EDITOR {file}";
+      }
+      {
+        invocation = "create {subpath}";
+        execution = "$EDITOR {directory}/{subpath}";
+      }
+      {
+        invocation = "view";
+        execution = "less {file}";
+      }
       {
         key = "ctrl-j";
         execution = ":line_down";
