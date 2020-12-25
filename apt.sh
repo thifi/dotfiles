60a9f5feb9f9c4d8e79511073260af06615e8f39
Itay Garin <thifixp@gmail.com>
2020-12-25 15:10:01 +0200

    apt: Move buggy Nix installs to apt.sh

---
+++ apt.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# These aren't installed by home-manager 
+# due to weird bugs....
+
+sudo apt install google-chrome-stable # tiny mouse cursor @home-manager
+sudo apt install keepass2 # aweful and unreadable colorschema @home-manager
+
+# On a PC
+# sudo apt install regolith-desktop-standard
+
+# On a Laptop
+sudo apt install regolith-desktop-mobile
