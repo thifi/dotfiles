11c173af984a1ed50949afa9e9a3b533b395f849
Itay Garin <thifixp@gmail.com>
2021-01-09 21:56:34 +0200

    home-manager: fish: Add missing foreign-env plugin

--- configs/common/home-manager/.config/nixpkgs/home.nix
+++ configs/common/home-manager/.config/nixpkgs/home.nix
@@ -94,6 +94,15 @@
         };
       }
       {
+        name = "plugin-foreign-env";
+        src = pkgs.fetchFromGitHub {
+          owner = "oh-my-fish";
+          repo = "plugin-foreign-env";
+          rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
+          sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs"; 
+        };
+      }
+      {
         name = "aws";
         src = pkgs.fetchFromGitHub {
           owner = "oh-my-fish";
