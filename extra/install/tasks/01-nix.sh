4a04c14559f790ebf7fce08f0c03c8759a7fa8b6
Itay Garin <thifixp@gmail.com>
2021-05-14 22:38:35 +0300

    extra: install: Fix 01-nix.sh

--- extra/install/tasks/01-nix.sh
+++ extra/install/tasks/01-nix.sh
@@ -4,7 +4,7 @@ set -e
 
 curl -L "https://nixos.org/nix/install" | sh
 
-# echo "if [ -e /home/$USER/.nix-profile/etc/profile.d/nix.sh ]; then . /home/$USER/.nix-profile/etc/profile.d/nix.sh; fi" >> ~/.bashrc
+echo "if [ -e /home/$USER/.nix-profile/etc/profile.d/nix.sh ]; then . /home/$USER/.nix-profile/etc/profile.d/nix.sh; fi" >> ~/.bashrc
 . /home/$USER/.nix-profile/etc/profile.d/nix.sh
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
